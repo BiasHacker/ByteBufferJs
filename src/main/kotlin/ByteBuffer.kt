5a5117838006e8bde59dd4cb10393e2132f2c56b
BiasHacker <altria.agar@gmail.com>
2018-12-15 03:07:40 +0900

    変数/関数名の調整

--- src/main/kotlin/ByteBuffer.kt
+++ src/main/kotlin/ByteBuffer.kt
@@ -7,28 +7,34 @@ class ByteBuffer {
     var bytes: MutableList<Byte> = mutableListOf()
     var position: Int = 0
 
+    @JsName("size")
     val size: Int
         get() = bytes.size
 
+    @JsName("clear")
     fun clear() {
         position = 0
         bytes.clear()
     }
 
+    @JsName("readBytes")
     fun readBytes(len: Int): List<Byte> {
         val data = bytes.subList(position, position + len)
         position += len
         return data
     }
 
+    @JsName("readByte")
     fun readByte(): Byte {
         return bytes[position++]
     }
 
+    @JsName("readBoolean")
     fun readBoolean(): Boolean {
         return readByte().toInt() > 0
     }
 
+    @JsName("readDouble")
     fun readDouble(): Double {
         var value = 0UL
         for (i in 56 downTo 0 step 8) {
@@ -37,6 +43,7 @@ class ByteBuffer {
         return Double.fromBits(value.toLong())
     }
 
+    @JsName("readFloat")
     fun readFloat(): Float {
         var value = 0
         for (i in 24 downTo 0 step 8) {
@@ -45,6 +52,7 @@ class ByteBuffer {
         return Float.fromBits(value)
     }
 
+    @JsName("readInt")
     fun readInt(): Int {
         var value = 0
         for (i in 24 downTo 0 step 8) {
@@ -53,6 +61,7 @@ class ByteBuffer {
         return value
     }
 
+    @JsName("readShort")
     fun readShort(): Short {
         var value: Short = 0
         for (i in 8 downTo 0 step 8) {
@@ -62,40 +71,49 @@ class ByteBuffer {
         return value
     }
 
+    @JsName("readUnsignedByte")
     fun readUnsignedByte(): UByte {
         return readByte().toUByte()
     }
 
+    @JsName("readUnsignedInt")
     fun readUnsignedInt(): UInt {
         return readInt().toUInt()
     }
 
+    @JsName("readUnsignedShort")
     fun readUnsignedShort(): UShort {
         return readShort().toUShort()
     }
 
+    @JsName("readUTF")
     fun readUTF(): String {
         return readUTFBytes(readShort().toInt())
     }
 
+    @JsName("readUTFBytes")
     fun readUTFBytes(length: Int): String {
         return readBytes(length).toUTF8String()
     }
 
+    @JsName("writeBytes")
     fun writeBytes(value: List<Byte>) {
         bytes.addAll(value)
         position += value.size
     }
 
+    @JsName("writeByte")
     fun writeByte(value: Byte) {
         bytes.add(value)
         position += 1
     }
 
+    @JsName("writeBoolean")
     fun writeBoolean(value: Boolean) {
         writeByte(if (value) 1 else 0)
     }
 
+    @JsName("writeDouble")
     fun writeDouble(value: Double) {
         val intBits = value.toBits()
         for (i in 56 downTo 0 step 8) {
@@ -103,6 +121,7 @@ class ByteBuffer {
         }
     }
 
+    @JsName("writeFloat")
     fun writeFloat(value: Float) {
         val intBits = value.toBits()
         for (i in 24 downTo 0 step 8) {
@@ -110,30 +129,35 @@ class ByteBuffer {
         }
     }
 
+    @JsName("writeInt")
     fun writeInt(value: Int) {
         for (i in 24 downTo 0 step 8) {
             writeByte((value shr i and 0xFF).toByte())
         }
     }
 
+    @JsName("writeShort")
     fun writeShort(value: Short) {
         for (i in 8 downTo 0 step 8) {
             writeByte((value shr i and 0xFF).toByte())
         }
     }
 
+    @JsName("writeUnsignedInt")
     fun writeUnsignedInt(value: UInt) {
         for (i in 24 downTo 0 step 8) {
             writeByte((value shr i and 0xFFU).toByte())
         }
     }
 
+    @JsName("writeUTF")
     fun writeUTF(value: String) {
         val hex = value.toUTF8Bytes()
         writeShort(hex.size.toShort())
         writeBytes(hex)
     }
 
+    @JsName("writeUTFBytes")
     fun writeUTFBytes(value: String) {
         writeBytes(value.toUTF8Bytes())
     }
